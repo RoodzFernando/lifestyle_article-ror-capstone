0094a05376ce2146d9325e48a8aed99f77703344
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-23 17:12:22 -0400

    Add the associations and validations for article, user models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,18 @@
 class Article < ApplicationRecord
+    belongs_to :user, foreign_key: "author_id"
+    has_one_attached :image
+    has_one :category, foreign_key: "category_id"
+    accepts_nested_attributes_for :category
+    has_many :votes
+
+    # scope :most_voted, -> { Article.joins(:votes).group("articles.title").count("articles.id").max }
+    def self.most_voted
+        # Article.joins(:votes).group("articles.title").count("articles.id")
+        find_by_sql("SELECT title, text, count(articles.id) AS vote_count FROM articles
+                        INNER JOIN votes
+                        on articles.id = votes.article_id GROUP BY articles.id ORDER by vote_count DESC LIMIT 1")
+    end
+
+    validates :title, presence: true, length: {maximum: 150}
+    validates :text, presence: true
 end
