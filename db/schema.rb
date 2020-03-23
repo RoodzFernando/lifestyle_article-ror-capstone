3ac5738ec6f3f0397c1958c3a524183d64c1e655
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-23 18:29:52 -0400

    Generate the vote model and add the associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_221513) do
+ActiveRecord::Schema.define(version: 2020_03_23_222634) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "author_id"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2020_03_23_221513) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
