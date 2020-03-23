0595122eedc1c31f295aa7cbcde72f99ecc54fe5
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-23 18:19:41 -0400

    Add category_id in the articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_215003) do
+ActiveRecord::Schema.define(version: 2020_03_23_221513) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "author_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_215003) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
