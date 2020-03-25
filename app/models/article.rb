1fc6861956945c351b1e319cc24b63b6510d00a3
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-25 13:44:52 -0400

    Fix bugs in the article modle

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, validate_media_type: false
     # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     belongs_to :user, foreign_key: "author_id"
