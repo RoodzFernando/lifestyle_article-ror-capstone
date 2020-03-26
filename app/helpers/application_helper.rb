0d6389a820a065a7666f0fe2afc75676411d7685
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-26 17:50:54 -0400

    Finish with the homepage styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')
+        base_title = "Recipe Book"
+        if page_title.empty?
+        base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
