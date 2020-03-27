d31ac57910ae98028f60475bfc66df5ad99f9109
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-27 19:07:44 -0400

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,7 @@ class Category < ApplicationRecord
     end
 
     scope :category_article, -> {
-        Article.find_by_sql("SELECT DISTINCT *,categories.priority,MAX(articles.created_at) FROM categories
+        Article.find_by_sql("SELECT DISTINCT *,articles.id, categories.priority,MAX(articles.created_at) FROM categories
                     INNER JOIN articles on articles.category_id = categories.id
                     GROUP by categories.priority, categories.id
                     ORDER by categories.priority ASC, MAX(articles.created_at) DESC LIMIT 4")}
