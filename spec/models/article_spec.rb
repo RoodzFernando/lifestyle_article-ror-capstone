b308147673b8c45d13994f20a733b0712766f985
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-29 10:46:37 -0400

    Write tests for the models

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+    context "Validation for the article" do
+        it "Ensure title presence" do
+            article = Article.new(title: "", text: "This is an article").save
+            expect(article).to eq(false)
+        end
+
+        it "Ensure text presence" do
+            article = Article.new(title: "A good article", text: "").save
+            expect(article).to eq(false)
+        end
+
+        it "Ensure the maximum title's length" do
+            article = Article.new(title: "title"*146, text: "text").save
+            expect(article).to eq(false)
+        end
+
+        it "should save successfully" do
+            article = Article.new(title: "A good article", text: "with an awesome content", category_id: 1).save
+            expect(article).to eq(true)
+        end
+    end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    context "Validation for the user" do
+        it "Ensure name presence" do
+            user = User.new(name: "").save
+            expect(user).to eq(false)
+        end
+
+        it "Ensure the minimum name's length" do
+            user = User.new(name: "Hey").save
+            expect(user).to eq(false)
+        end
+
+        it "should save successfully" do
+            user = User.new(name: "user_01").save
+            expect(user).to eq(true)
+        end
+    end
+end
