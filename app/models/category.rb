868943be7e3fa133b3f59a9a03aeb741d793a9b2
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-30 13:43:48 -0400

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,7 @@ class Category < ApplicationRecord
   scope :category_article, -> {
       # Article.select('categories.* ,articles.*').joins('INNER JOIN categories ON categories.id = articles.category_id').where('categories.name').distinct.group('categories.id').order('priority').limit(4)
 
-    Article.find_by_sql("SELECT DISTINCT articles.category_id, articles.image_file_name, articles.title,categories.name, categories.id  FROM articles INNER JOIN categories ON articles.category_id = categories.id
+    Article.find_by_sql("SELECT DISTINCT articles.category_id, articles.image_file_name,categories.priority, articles.title,categories.name, categories.id  FROM articles INNER JOIN categories ON articles.category_id = categories.id
     GROUP BY categories.priority ORDER BY categories.priority LIMIT 4 ")
   }
     end
