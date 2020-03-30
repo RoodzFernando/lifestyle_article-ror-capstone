356259b618047b1d5d487a9b79f276e3b757cf49
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-30 08:35:40 -0400

    Fix the query for the categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,9 +5,5 @@ class Category < ApplicationRecord
     end
 
     scope :category_article, -> {
-        Article.find_by_sql("SELECT DISTINCT *,articles.id, categories.priority,MAX(articles.created_at) FROM categories
-                    INNER JOIN articles on articles.category_id = categories.id
-                    GROUP by categories.priority, categories.id
-                    ORDER by categories.priority ASC, MAX(articles.created_at) DESC LIMIT 4")}
-
-end
+        Article.select("articles.*, categories.*").joins('INNER JOIN categories ON categories.id = articles.category_id').order('categories.priority').group('categories.id').limit(4)}
+    end
