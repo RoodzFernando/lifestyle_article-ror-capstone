930ee40935aa20b627a17ddadc6322d85cf3d7a8
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-30 18:44:27 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
 
   def logged_in_user
     return if logged_in?
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
+
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
 end
