733c41f970626cbc396c480e038231ba9a0db5bc
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-30 18:43:02 -0400

    Fix the stickler errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def logged_in_user
-    unless logged_in?
+    return if logged_in?
       flash[:danger] = 'Please log in.'
       redirect_to login_url
-    end
   end
 end
