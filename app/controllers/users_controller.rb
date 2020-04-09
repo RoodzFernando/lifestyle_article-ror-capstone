b6d7d68dd8e4bb7f9071d244e62feb6fe0efcbda
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-04-09 13:06:10 -0400

    Update users_controller and sessions new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      log_in @user
       flash[:success] = 'Thanks for registering!'
+      redirect_to root_url
     else
       render 'new'
     end
